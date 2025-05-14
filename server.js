8b96c8c55b5fc7a7566a871c8a25ac39c40add05
Kacper Łempicki <kacperlempicki125@wp.pl>
2025-05-14 21:43:35 +0200

    Last railway attempt...mby

--- server.js
+++ server.js
@@ -50,6 +50,11 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
+// Obsługa dodatkowych ścieżek
+app.get(['/index.html', '/main-page'], (req, res) => {
+	res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 // Obsługa specyficznych ścieżek, jeśli używasz routingu po stronie klienta
 app.get('/index.html', (req, res) => {
 	res.sendFile(path.join(__dirname, 'dist', 'index.html'));
