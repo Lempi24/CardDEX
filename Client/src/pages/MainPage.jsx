f3d0bef296b50b7cabf51acb1aa9044de7402f21
Kacper Łempicki <kacperlempicki125@wp.pl>
2025-06-30 15:20:47 +0200

    Log out added

--- Client/src/pages/MainPage.jsx
+++ Client/src/pages/MainPage.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import CardBtn from '../components/CardBtn';
 import logo from '../img/pokeball.png';
 import pikachu from '../img/cards/pikachu.png';
@@ -19,6 +20,7 @@ const MainPage = () => {
 	const [isCardInfoVisiable, setIsCardInfoVisiable] = useState(false);
 	const [selectedCard, setSelectedCard] = useState(null);
 	const [isCameraVisiable, setIsCameraVisiable] = useState(false);
+	const navigate = useNavigate();
 	const handleCardClick = (id) => {
 		return () => {
 			setIsCardInfoVisiable((prevState) => !prevState);
@@ -31,11 +33,32 @@ const MainPage = () => {
 	const handleAddCard = () => {
 		setIsCameraVisiable(true);
 	};
+	const handleLogOut = () => {
+		if (localStorage.getItem('token')) {
+			localStorage.removeItem('token');
+			navigate('/');
+		}
+	};
 	return (
 		<main className='relative bg-main min-h-screen w-full text-text flex items-center justify-center'>
-			<div className='flex items-center justify-center w-full pt-4 absolute top-0'>
-				<img src={logo} alt='pokeball logo' className='w-10 h-10' />
-				<h2 className='ml-3 text-xl font-bold'>CardDEX</h2>
+			<div className='absolute top-0 flex items-center justify-around w-full pt-4'>
+				<div className='flex items-center justify-center'>
+					<img src={logo} alt='pokeball logo' className='w-10 h-10' />
+					<h2 className='ml-3 text-xl font-bold'>CardDEX</h2>
+				</div>
+				<button
+					className='flex items-center gap-2 text-accent1'
+					onClick={handleLogOut}
+				>
+					<p className=''>Log out</p>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						viewBox='0 0 512 512'
+						className='w-[30px] fill-accent1'
+					>
+						<path d='M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z' />
+					</svg>
+				</button>
 			</div>
 			<CardInfo
 				visible={isCardInfoVisiable}
