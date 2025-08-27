0bfa769f3e1d7b63ea9fef3a80c964d3e855f168
Kacper Łempicki <kacperlempicki125@wp.pl>
2025-08-27 12:59:19 +0200

    vite config changes

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -13,4 +13,12 @@ export default defineConfig({
 			'e1a9-37-248-218-81.ngrok-free.app',
 		],
 	},
+	optimizeDeps: {
+		exclude: ['heic2any'],
+	},
+	build: {
+		rollupOptions: {
+			external: ['heic2any'],
+		},
+	},
 });
